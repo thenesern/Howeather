f5ee5f61f6b1dc652823adea7eadcfd34bbd7ad0
Enes Eren <88148105+thenesern@users.noreply.github.com>
2022-01-30 13:36:44 +0300

    Added Main Component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import styles from "./App.module.css";
 // Components
 import Header from "./components/Header/Header.js";
+import Main from "./components/Main/Main.js";
 
 function App() {
   return (
     <div id={styles.weatherApp}>
       <Header />
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main/Main.js
@@ -0,0 +1,44 @@
+// Packages
+import { useState } from "react";
+// Styles
+import styles from "./Main.module.css";
+// Components
+import InputAPI from "../../InputAPI";
+
+function Main() {
+  const [inputCity, setInputCity] = useState("Ankara");
+  const [input, setInput] = useState();
+  const [inputValue, setInputValue] = useState("");
+
+  function citySubmitHandler(e) {
+    e.preventDefault();
+    setInputCity(input);
+    setInputValue("");
+  }
+
+  function inputChangeHandler(e) {
+    setInput(e.target.value);
+    setInputValue(e.target.value);
+  }
+
+  return (
+    <main>
+      <form className={styles.inputContainer} onSubmit={citySubmitHandler}>
+        <label htmlFor="search">Search a city:</label>
+        <input
+          type="text"
+          id="input"
+          className={styles.search}
+          onChange={inputChangeHandler}
+          value={inputValue}
+        />
+        <input type="submit" className={styles.glass} value="🔍" />
+      </form>
+      <div id="output" className={styles.outputContainer}>
+        <InputAPI cityName={inputCity} />
+      </div>
+    </main>
+  );
+}
+
+export default Main;
